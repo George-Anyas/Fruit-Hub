d8febdc683ed87b5a3ee8ee84fe6222cb780748e
GeorgeSoliman <georgelove462@gmail.com>
2024-11-17 18:16:56 +0200

    add git it

--- lib/features/auth/presentation/views/signup_view.dart
+++ lib/features/auth/presentation/views/signup_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_app/core/services/get_it_service.dart';
 import 'package:fruit_app/core/widgets/custom_appbar.dart';
+import 'package:fruit_app/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruit_app/features/auth/presentation/cubit/signup_cubit/signup_cubit.dart';
 import 'package:fruit_app/features/auth/presentation/views/widgets/signup_view_body.dart';
 
 class SignupView extends StatelessWidget {
@@ -7,9 +11,14 @@ class SignupView extends StatelessWidget {
   static const routeName = '/signup';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'حساب جديد'),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(
+        getIt<AuthRepo>(),
+      ),
+      child: Scaffold(
+        appBar: CustomAppBar(title: 'حساب جديد'),
+        body: SignupViewBody(),
+      ),
     );
   }
 }
