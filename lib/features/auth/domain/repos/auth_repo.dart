69035dd32f57e023d6557853be0fb9e16b4a6e8e
GeorgeSoliman <georgelove462@gmail.com>
2024-11-17 14:40:31 +0200

    add dart z package

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -1,6 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruit_app/core/errors/failures.dart';
 import 'package:fruit_app/features/auth/domain/entites/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<UserEntity> createUserwithemailandpassword(
+  Future<Either<Failure, UserEntity>> createUserwithemailandpassword(
       {required String email, required String password});
 }
